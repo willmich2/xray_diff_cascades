bc495de845ad560414ec0585bd4fa11869f304ac
Will <willmich@mit.edu>
2026-08-17 15:19:30 -0400

    change denominator to 2.5

--- paper/experiments/fzp_cascade_comparison.py
+++ paper/experiments/fzp_cascade_comparison.py
@@ -242,7 +242,7 @@ def main() -> None:
     }
     opt_params = {
         "Nelem": Nelem,
-        "min_feature_size": min_feature_size / 3.0,
+        "min_feature_size": min_feature_size / 2.5,
         "epsilon": epsilon,
         "tolerance": tolerance,
         "param_tolerance": param_tolerance,
